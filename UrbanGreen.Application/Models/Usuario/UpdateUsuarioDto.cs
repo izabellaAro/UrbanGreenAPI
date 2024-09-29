3f51a7d61363e6661f1fb362185c5b68a2e74cc5
GabriellaBenevides <gabriellabenevides22@gmail.com>
2024-09-29 14:33:45 -0300

    CRUD em usuário

---
+++ UrbanGreen.Application/Models/Usuario/UpdateUsuarioDto.cs
@@ -0,0 +1,9 @@
+﻿namespace UrbanGreen.Application.Models.Usuario
+{
+    public class UpdateUsuarioDto
+    {
+            public string NomeUsuario { get; set; }
+            public string Email { get; set; }
+            public string NovaSenha { get; set; }
+    }
+}
--- UrbanGreenAPI/Controllers/LoginController.cs
+++ UrbanGreenAPI/Controllers/LoginController.cs
@@ -56,6 +56,82 @@ public class LoginController : ControllerBase
 
         return Ok();
     }
+    [Authorize(Roles = "Admin")]
+    [HttpGet("usuarios")]
+    public async Task<IActionResult> ObterUsuarios()
+    {
+        var usuarios = _userManager.Users.Select(user => new
+        {
+            user.Id,
+            user.UserName,
+            user.Email
+        }).ToList();
+
+        return Ok(usuarios);
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpGet("usuarios/{id}")]
+    public async Task<IActionResult> ObterUsuarioPorId(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+
+        if (user == null)
+            return NotFound();
+
+        return Ok(new
+        {
+            user.Id,
+            user.UserName,
+            user.Email
+        });
+    }
+    [Authorize(Roles = "Admin")]
+    [HttpPut("usuarios/{id}")]
+    public async Task<IActionResult> EditarUsuario(string id, [FromBody] UpdateUsuarioDto model)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+
+        if (user == null)
+            return NotFound();
+
+        user.UserName = model.NomeUsuario;
+        user.Email = model.Email;
+
+        var result = await _userManager.UpdateAsync(user);
+
+        if (!result.Succeeded)
+            return BadRequest(result.Errors);
+
+        if (!string.IsNullOrEmpty(model.NovaSenha))
+        {
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var passwordResult = await _userManager.ResetPasswordAsync(user, token, model.NovaSenha);
+
+            if (!passwordResult.Succeeded)
+                return BadRequest(passwordResult.Errors);
+        }
+
+        return Ok();
+    }
+    [Authorize(Roles = "Admin")]
+    [HttpDelete("usuarios/{id}")]
+    public async Task<IActionResult> ExcluirUsuario(string id)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+
+        if (user == null)
+            return NotFound();
+
+        var result = await _userManager.DeleteAsync(user);
+
+        if (!result.Succeeded)
+            return BadRequest(result.Errors);
+
+        return Ok();
+    }
+
+
 
     private async Task<string> GerarJwtToken(Usuario user)
     {
