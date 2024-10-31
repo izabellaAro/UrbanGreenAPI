b164a1c765b0e0252e188a1df7bde5fc8bb32172
Izabella Aro <izabellaaro20@gmail.com>
2024-10-30 23:50:15 -0300

    fix: Adiciona dto de criacao de item pedido

---
+++ UrbanGreen.Application/Models/ItemPedido/CreateItemPedidoDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrbanGreen.Application.Models.ItemPedido;
+
+public class CreateItemPedidoDto
+{
+    [Required(ErrorMessage = "A quantidade é obrigatória.")]
+    [Range(1, int.MaxValue, ErrorMessage = "A quantidade deve ser maior que zero.")]
+    public int Quantidade { get; set; }
+    [Required]
+    public int ProdutoId { get; set; }
+}
